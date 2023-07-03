d696e15debda33eef4a36e1dda6126b83718ba05
Nadeem Yaseen <86344264+nadeemyaseen-rs@users.noreply.github.com>
2023-07-03 20:46:45 +0500

    Update install_ubuntu_dependencies_build.sh to gcc11

--- .github/scripts/install_ubuntu_dependencies_build.sh
+++ .github/scripts/install_ubuntu_dependencies_build.sh
@@ -1,12 +1,13 @@
 # Install required dependencies for Ubuntu systems
 sudo apt-get update -qq
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt install -y \
-  g++-9 \
+  g++-11 gcc-11 \
   cmake \
   build-essential \
   libreadline-dev \
   ninja-build 
   
-sudo ln -sf /usr/bin/g++-9 /usr/bin/g++
-sudo ln -sf /usr/bin/gcc-9 /usr/bin/gcc
-sudo ln -sf /usr/bin/gcov-9 /usr/bin/gcov
+sudo ln -sf /usr/bin/g++-11 /usr/bin/g++
+sudo ln -sf /usr/bin/gcc-11 /usr/bin/gcc
+sudo ln -sf /usr/bin/gcov-11 /usr/bin/gcov
